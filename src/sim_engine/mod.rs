3277952949d3b0d3bba70b9b63835fffe7f75917
Bernd <bernd.durrwachter@gmail.com>
2024-10-27 09:47:14 +0100

    added engine & game modules

---
+++ src/sim_engine/mod.rs
@@ -0,0 +1,7 @@
+// SIMULATION ENGINE Rust module for SimWorldAS-Prototype
+// BD 2024-10-27 : Initial Version (adapted from SimWorld24-Rust)
+
+// central config for all /sim_engine/ folder modules...
+// - Sim Loop
+// - Time Progress & Aggregation
+// - System Integration
---
+++ src/sim_game/mod.rs
@@ -0,0 +1,6 @@
+// AS GAME Rust module for SimWorldAS-Prototype
+// BD 2024-10-27 : Initial Version (adapted from SimWorld24-Rust)
+
+// central config for all /sim_game/ folder modules...
+// - User Experience
+// - Game Flow
