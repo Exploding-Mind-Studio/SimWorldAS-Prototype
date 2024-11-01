77a75079eeac81bd47c85dfe07894dd7522a0840
Bernd <bernd.durrwachter@gmail.com>
2024-11-01 08:25:13 +0100

    new concept_model

---
+++ src/sim_model/concept_model/mod.rs
@@ -0,0 +1,27 @@
+// CONCEPT MODEL Rust module for SimWorldAS-Prototype
+// BD 2024-11-01 : Initial Version 
+
+// PURPOSE / SCOPE:
+// - Concepts in the minds, recognition, cultures of SimHumans...
+// - E.g. Houses, Paths, Locations, Settlements (Towns), Friends/Enemies, Fields
+// Distinction between the designed natural world, and the perceived world by NPC Creatures
+// - with some kind of matching between perceived concepts and the natural manifestation
+
+// Example prototypes..
+
+use crate::sim_model::{
+    world_model::*,
+    entity_model::*
+};
+
+pub struct House{}
+pub struct Settlement{}
+pub struct Path{}
+
+pub struct People{}
+pub struct Friend{}
+pub struct Enemy{}
+pub struct Predator{}
+pub struct Prey{}
+
+
